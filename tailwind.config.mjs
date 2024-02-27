3bb547564da13e94acd295bd6ebc8cc8b3a9f7d1
Jesus-Puertos <226w0496@zongolica.tecnm.mx>
2024-02-27 01:08:25 -0600

    agregacion de enlaces de redes y boton para cambiar de color de la pagina

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -3,9 +3,15 @@ import animations from "@midudev/tailwind-animations";
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  darkMode: "class", // or 'media' or 'class'
   theme: {
     extend: {
       colors: {
+        //modo claro
+        primaryClaro: "#FF6363",
+        secondaryClaro: "#4a5568",
+
+        //modo oscuro
         primary: "#FF6363",
         secondary: "#ddd",
       },
